30978d65d5badecbf168ecf2d9dbddf6d2897fdd
Vasyl Khalaim <vasyl.khalaim@ukr.net>
2026-03-04 16:36:58 +0200

    chrome

--- shop-test.cjs
+++ shop-test.cjs
@@ -51,6 +51,7 @@ async function captureReport() {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: "/usr/bin/google-chrome",
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
